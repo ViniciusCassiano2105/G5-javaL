ad638cf251ffd09bbe3008d01147e014628e7839
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-05 11:38:26 -0300

    Classe Carrinho

---
+++ projeto_poo/src/br/com/vidaadultafacil/pedidos/Carrinho.java
@@ -0,0 +1,8 @@
+package br.com.vidaadultafacil.pedidos;
+
+public class Carrinho {
+	public static void main(String[] args) {
+		
+		
+	}
+}
--- projeto_poo/src/br/com/vidaadultafacil/pedidos/Sistema.java
+++
@@ -1,5 +0,0 @@
-package br.com.projeto.principal;
-
-public class Sistema {
-
-}
