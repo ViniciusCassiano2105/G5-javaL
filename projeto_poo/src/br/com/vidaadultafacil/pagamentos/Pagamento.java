d8cb26e0d3addb231c6c7da2382d4cd6ee6f55a1
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-05 11:49:52 -0300

    Pacote Pagamentos/ Classe pagamento

---
+++ projeto_poo/src/br/com/vidaadultafacil/pagamentos/Pagamento.java
@@ -0,0 +1,20 @@
+package br.com.vidaadultafacil.pagamentos;
+
+import java.math.BigDecimal;
+
+public class Pagamento {
+
+	public static void main(String[] args) {
+		
+		// Método de pagamento utilizado (ex: "cartão de crédito", "boleto")
+		String metodoPagamento;
+		
+		// Valor total do pagamento
+		BigDecimal valor;
+		
+		// Quantidade de parcelas do pagamento
+		int parcela;
+
+	}
+
+}
