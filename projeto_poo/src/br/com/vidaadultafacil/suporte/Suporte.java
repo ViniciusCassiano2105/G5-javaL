a55d8490f7058638c6461d4865015bc0d6861967
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-05 11:55:22 -0300

    Pacote Suporte/ Classe Suporte

---
+++ projeto_poo/src/br/com/vidaadultafacil/suporte/Suporte.java
@@ -0,0 +1,19 @@
+package br.com.vidaadultafacil.suporte;
+
+import br.com.vidaadultafacil.usuarios.Cliente;
+
+public class Suporte {
+
+	public static void main(String[] args) {
+		
+		// Usuário que enviou a mensagem de suporte
+		Cliente usuario;
+		
+		// Mensagem enviada pelo usuário solicitando suporte
+		String mensagem;
+		
+		
+
+	}
+
+}
