7fd58a08de74f8aa9ac96f17f460b9c877a094e6
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-05 11:22:30 -0300

    Classe Administrador

---
+++ projeto_poo/src/br/com/vidaadultafacil/usuarios/Administrador.java
@@ -0,0 +1,15 @@
+package br.com.vidaadultafacil.usuarios;
+
+public class Administrador {
+
+	public static void main(String[] args) {
+	
+	String nome;
+	String email;
+	String senha;
+	String telefone;
+	
+
+	}
+
+}
