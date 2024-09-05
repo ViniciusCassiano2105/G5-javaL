f5ba61711b041b730637c21e829ee126493c17e9
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-05 12:01:05 -0300

    Classe Relatorio Estoque

---
+++ projeto_poo/src/br/com/vidaadultafacil/relatorios/RelatorioEstoque.java
@@ -0,0 +1,11 @@
+package br.com.vidaadultafacil.relatorios;
+
+public class RelatorioEstoque {
+
+	public static void main(String[] args) {
+		
+		// Quantidade total de produtos no estoque
+		int quantidadeProdutos;
+	}
+
+}
