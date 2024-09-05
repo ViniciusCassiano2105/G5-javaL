87e372761c9dcf5b3d225c4d5c5a9838bb4c2fac
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-05 12:03:03 -0300

    Comentários administrador

--- projeto_poo/src/br/com/vidaadultafacil/usuarios/Administrador.java
+++ projeto_poo/src/br/com/vidaadultafacil/usuarios/Administrador.java
@@ -4,9 +4,16 @@ public class Administrador {
 
 	public static void main(String[] args) {
 	
+	// Nome completo do administrador
 	String nome;
+	
+	// Endereço de email do administrador
 	String email;
+	
+	// Senha do administrador
 	String senha;
+	
+	// Número de telefone do administrador
 	String telefone;
 	
 
