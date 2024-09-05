25f591c17843c6dcba62ab2377fd75f579a4c449
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-05 11:44:20 -0300

    Classe histórico de compra

---
+++ projeto_poo/src/br/com/vidaadultafacil/pedidos/HistoricoCompra.java
@@ -0,0 +1,23 @@
+package br.com.vidaadultafacil.pedidos;
+
+import br.com.vidaadultafacil.tela_inicial.Produto;
+import br.com.vidaadultafacil.usuarios.Cliente;
+
+public class HistoricoCompra {
+
+	public static void main(String[] args) {
+		
+		// Identificação do pedido
+		String idPedido;
+		
+		// Usuário que realizou o pedido
+		Cliente usuario;
+		
+		// Lista de produtos comprados
+		Produto listadeproduto;
+		
+		// Status atual do pedido (ex: "em processamento", "entregue")
+		String status;
+	}
+
+}
