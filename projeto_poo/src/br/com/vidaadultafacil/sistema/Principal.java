730a8aa00bf051fd6e18b3d670fd2c7c0681de0e
UserCardinot <lucas.cardinot@outlook.com>
2024-09-12 15:31:34 -0300

    remove criacao de admin e cliente da principal

--- projeto_poo/src/br/com/vidaadultafacil/sistema/Principal.java
+++ projeto_poo/src/br/com/vidaadultafacil/sistema/Principal.java
@@ -1,30 +1,16 @@
 package br.com.vidaadultafacil.sistema;
 
-import java.io.IOException;
-
 import br.com.vidaadultafacil.io.LeitorArquivo;
-import br.com.vidaadultafacil.usuarios.Administrador;
-import br.com.vidaadultafacil.usuarios.Cliente;
+import java.io.IOException;
 
 public class Principal {
     public static void main(String[] args) {
     	
-    	  
     	try {
 			LeitorArquivo.leitor("Banco");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	
-	    
-        // Criando um cliente
-        Cliente cliente = new Cliente("Lucas Silva", "Lucas@gmail.com", "senha321", "(21) 99999-9999", "111.111.111-11");
-        
-        // Criando um admin
-        Administrador admin = new Administrador("Maria Admin", "maria.admin@gmail.com", "adminsenha", "45648791" ,true);
-        
-        System.out.println(cliente);
-        System.out.println(admin);
     } 
 }
