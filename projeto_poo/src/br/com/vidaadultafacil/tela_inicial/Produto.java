91e00ce726574f07c533e8f65fe21710510bd466
leticiapzs <leticia_95lima@hotmail.com>
2024-09-13 16:29:19 -0300

    Inserção de if else em Produto

--- projeto_poo/src/br/com/vidaadultafacil/tela_inicial/Produto.java
+++ projeto_poo/src/br/com/vidaadultafacil/tela_inicial/Produto.java
@@ -4,82 +4,99 @@ import java.math.BigDecimal;
 
 public class Produto {
 
-	// Chave primária
-	private int idProduto;
+    // Chave primária
+    private int idProduto;
 
-	public String nome = "";
-	public String descricaoProd = "";
-	public String autor = "";
-	public BigDecimal preco;
-	public int duracao;
+    public String nome = "";
+    public String descricaoProd = "";
+    public String autor = "";
+    public BigDecimal preco;
+    public int duracao;
 
-	public Produto() {
-	}
+    public Produto() {
+    }
 
-	public Produto(int idProduto, int fkCategoria, String nome, String descricaoProd, String autor, BigDecimal preco, int duracao) {
-		this.idProduto = idProduto;
-		this.nome = nome;
-		this.descricaoProd = descricaoProd;
-		this.autor = autor;
-		this.preco = preco;
-		this.duracao = duracao;
-	}
+    public Produto(int idProduto, int fkCategoria, String nome, String descricaoProd, String autor, BigDecimal preco, int duracao) {
+        this.idProduto = idProduto;
+        this.nome = nome;
+        this.descricaoProd = descricaoProd;
+        this.autor = autor;
+        this.preco = preco;
+        this.duracao = duracao;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public String getDescricaoProd() {
-		return descricaoProd;
-	}
+    public String getDescricaoProd() {
+        return descricaoProd;
+    }
 
-	public String getAutor() {
-		return autor;
-	}
+    public String getAutor() {
+        return autor;
+    }
 
-	public BigDecimal getPreco() {
-		return preco;
-	}
+    public BigDecimal getPreco() {
+        return preco;
+    }
 
-	public int getDuracao() {
-		return duracao;
-	}
+    public int getDuracao() {
+        return duracao;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setNome(String nome) {
+        if (nome != null && !nome.trim().isEmpty()) {
+            this.nome = nome;
+        } else {
+            System.out.println("O nome do produto não pode ser vazio.");
+        }
+    }
 
-	public void setDescricaoProd(String descricaoProd) {
-		this.descricaoProd = descricaoProd;
-	}
+    public void setDescricaoProd(String descricaoProd) {
+        if (descricaoProd != null && !descricaoProd.trim().isEmpty()) {
+            this.descricaoProd = descricaoProd;
+        } else {
+            System.out.println("A descrição do produto não deve ser vazio");
+        }
+    }
 
-	public void setAutor(String autor) {
-		this.autor = autor;
-	}
+    public void setAutor(String autor) {
+        if ( autor != null && !autor.trim().isEmpty()) {
+            this.autor = autor;
+        } else {
+            System.out.println("O nome do autor não deve ser vazio.");
+        }
+    }
 
-	public void setPreco(BigDecimal preco) {
-		this.preco = preco;
-	}
+    public void setPreco(BigDecimal preco) {
+        if (preco.compareTo(BigDecimal.ZERO) > 0 ) {
+            this.preco = preco;
+        } else {
+            System.out.println("O preço deve ser maior do que zero.");
+        }
 
-	public void setDuracao(int duracao) {
-		this.duracao = duracao;
-	}
+    }
 
-	public int getIdProduto() {
-		return idProduto;
-	}
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
 
-	public void setId(int idProduto) {
-		this.idProduto = idProduto;
-	}
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setId(int idProduto) {
+        this.idProduto = idProduto;
+    }
 
 
-	@Override
-	public String toString() {
-		return "Produto [idProduto=" + idProduto + ", fkCategoria=" +  ", nome=" + nome
-				+ ", descricaoProd=" + descricaoProd + ", autor=" + autor + ", preco=" + preco + ", duracao=" + duracao
-				+ "]";
-	}
-	
+    @Override
+    public String toString() {
+        return "Produto [idProduto=" + idProduto + ", fkCategoria=" +  ", nome=" + nome
+                + ", descricaoProd=" + descricaoProd + ", autor=" + autor + ", preco=" + preco + ", duracao=" + duracao
+                + "]";
+    }
+
 
 }
