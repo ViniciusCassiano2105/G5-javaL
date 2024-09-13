1373328cb258d2f1d743367859a8127c720a7b8a
leticiapzs <leticia_95lima@hotmail.com>
2024-09-13 18:43:41 -0300

    adição Map Suporte

--- projeto_poo/src/br/com/vidaadultafacil/suporte/Suporte.java
+++ projeto_poo/src/br/com/vidaadultafacil/suporte/Suporte.java
@@ -1,25 +1,37 @@
 package br.com.vidaadultafacil.suporte;
+import java.util.HashMap;
+import java.util.Map;
 
-import br.com.vidaadultafacil.usuarios.Cliente;
 
 public class Suporte {
 
 	private int id;		
 	private Cliente usuario;	
 	private String mensagem;
-
+	private static final Map<Integer, Suporte> mapaSuporte = new HashMap();
+	
 	public Suporte(int id, Cliente usuario, String mensagem) {
 		this.id = id;
 		this.usuario = usuario;
 		this.mensagem = mensagem;
 	}
 
+	public Suporte(Cliente usuario, String mensagem) {
+		this.id = mapaSuporte.size() + 1;
+		this.usuario = usuario;
+		this.mensagem = mensagem;
+	}
+	
 	public String getMensagem() {
 		return mensagem;
 	}
 
 	public Cliente getUsuario() {
 		return usuario;
+	}
+	
+	public static Map <Integer, Suporte> getMapSuporte() {
+    	return mapaSuporte;
 	}
 
 	public void setMensagem(String mensagem) {
