84f9b3f74f75d4674273775f1bcb5ca15cbbee32
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-15 17:45:18 -0300

    Switch - Menu cliente

--- projeto_poo/src/br/com/vidaadultafacil/sistema/MenuCliente.java
+++ projeto_poo/src/br/com/vidaadultafacil/sistema/MenuCliente.java
@@ -11,14 +11,24 @@ public class MenuCliente {
 	private static Logger logger = Logger.getLogger(Util.class.getName());
 
 	public static void main(String[] args) {
-	
+		int escolha = 0;
 		Scanner scanner = new Scanner(System.in);
-
 
 		logger.info("\n\t\t\tMENU DO CLIENTE\n");
 		logger.info("(1) Visualizar Produtos Disponíveis");
 		logger.info("(2) Sair");
-	
-		
+
+//		while (escolha != 2) {
+////			switch (escolha) {
+//		case 1:
+//			visualizarprodutos;
+//			break;
+//		case 2:
+//			logger.info(Saindo...);
+//			break;
+//		default:
+//            logger.info("Opção inválida! Por favor, escolha novamente.");
+//		}
+		scanner.close();  
 	}
 }
